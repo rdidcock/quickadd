c0d5d428aa8f9e0bc22b831c14ad21b6360ddd6d
Martin Antos <49389495+inferense@users.noreply.github.com>
2022-12-13 15:42:27 +0100

    bump version

--- setup.py
+++ setup.py
@@ -50,6 +50,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/inferense/quickadd',
-    version='0.5.4',
+    version='0.5.7',
     zip_safe=False,
 )
